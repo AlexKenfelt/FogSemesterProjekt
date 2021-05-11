bbe21904df3e28fb2775a0ae66006f7e8965f4d4
Julius <PsychedelicCoder@users.noreply.github.com>
2021-05-11 15:10:19 +0200

    All calculations are done

--- src/main/java/web/commands/CalcCarport.java
+++ src/main/java/web/commands/CalcCarport.java
@@ -15,30 +15,51 @@ public class CalcCarport {
         int actualLength = 0;
         //Dette er minimums kravet for hvor meget plads der skal være mellem hver stolpe.
         int minimumPostSpacing = 310;
+        //Dette er bare en konstant.
+        int multiplier = 2;
         //dette er vores float som kun bliver brugt i mellem regningerne, denne bliver typecasted til int result længere nede.
-        float calc = 0;
+        float result = 0;
         //Her vælger vi og tage carportens indtastede længde og minuser det med, det stykke som den foreste og bagerste stolpe skal sidde inde.
         actualLength = ((int)length - frontPost - backPost);
         //Her tager vi minimums kravet til spacing mellem hver stolpe og dividerer det med den faktiske længde på vore carport
-        calc = (minimumPostSpacing / actualLength);
+        result = (minimumPostSpacing / actualLength);
         //her tager vi og runder result op til det tætteste hel tal.
-        Math.round(calc);
+        Math.round(result);
         //Her ganger vi med 2 så vi får begge sider med af carporten.
-        calc = (calc * 2);
+        result = (result * multiplier);
         //Nu typecaster vi vores result som er en float om til en int.
-        int result = (int)calc ;
-        //Detter er det endelige resultat.
-        return result;
+        int numberOfPosts = (int)result ;
+        //Dett her er det endelige resultat.
+        return numberOfPosts;
     }
 
-    public int calcBeams () {
+    //TODO: Evt. sætte en maks længde på de bjælker der er på lageret.
+    //TODO: fks. hvis længden på carporten er 800cm men vi kun tilbyder 600cm bjælker skal der være 2x af 600cm og 2x af 200cm.
+    public int calcBeams (double length) {
 
+        //Dette er bare en konstant.
+        int multiplier = 2;
+        //Dette er vores endelige resultat.
+        int numberOfBeams;
+        //Vi tager bare ganger længden med 2, så vi har begge sider af vores carport.
+        //Vi går ud fra at lageret altid har den længde bjælke på lager der skal bruges.
+        numberOfBeams = ((int)length * multiplier);
 
+    return numberOfBeams;
     }
 
+    //TODO: Kundens indtastede width til sin carport skal matche med den witdh vores spær har.
+    public int calcRafters (double width, double length) {
 
-    //Beams
+        //Dette er afstanden der skal være imellem hver spær.
+        double rafterSpacing = 0.55;
+        //Dette er længden af vores spær. Dette skal helst være lig med den længde der bliver givet til carporten.
+        int lengthOfRafters;
+        //Dette er det endelige resultat.
+        int numberOfRafters;
+        //Vi tager bare og dividerer længden med, den bestemte afstand der skal være, for at få antal spær.
+        numberOfRafters = ((int)length / (int)rafterSpacing );
 
-    //rafters
-
+        return numberOfRafters;
+    }
 }
