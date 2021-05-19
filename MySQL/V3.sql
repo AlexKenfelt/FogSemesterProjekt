07cbbee8eadc8280610a388ebdfacac23b3390a4
AlexKenfelt <cph-ak428@cphbusiness.dk>
2021-05-19 13:58:56 +0200

    opdateret database

--- MySQL/V3.sql
+++ MySQL/V3.sql
@@ -1,4 +1,3 @@
-DROP DATABASE IF EXISTS fog;
 CREATE DATABASE  IF NOT EXISTS `fog` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `fog`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
@@ -32,13 +31,10 @@ CREATE TABLE `orders` (
                           `status` enum('pending','confirmed','delivered') NOT NULL,
                           `user_id` int DEFAULT NULL,
                           `timestamp` timestamp NULL DEFAULT NULL,
-                          `shed_id` int DEFAULT NULL,
                           PRIMARY KEY (`id`),
                           KEY `fk_orders_users1_idx` (`user_id`),
-                          KEY `fk_shed_id_idx` (`shed_id`),
-                          CONSTRAINT `fk_orders_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-                          CONSTRAINT `fk_shed_id` FOREIGN KEY (`shed_id`) REFERENCES `orders` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+                          CONSTRAINT `fk_orders_users1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -47,7 +43,7 @@ CREATE TABLE `orders` (
 
 LOCK TABLES `orders` WRITE;
 /*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-INSERT INTO `orders` VALUES (1,500,800,'pending',2,'2021-05-05 13:15:55',NULL),(5,2,2,'pending',NULL,NULL,NULL),(6,3,4,'pending',NULL,NULL,NULL),(7,6,6,'pending',NULL,NULL,NULL),(8,5,5,'pending',NULL,NULL,NULL),(9,3,4,'pending',NULL,NULL,NULL),(10,3,4,'pending',NULL,NULL,NULL),(11,200,450,'pending',NULL,NULL,NULL);
+INSERT INTO `orders` VALUES (1,500,800,'confirmed',2,'2021-05-05 13:15:55'),(5,2,2,'confirmed',NULL,NULL),(6,3,4,'pending',NULL,NULL),(7,6,6,'confirmed',NULL,NULL),(8,5,5,'confirmed',NULL,NULL),(9,3,4,'confirmed',NULL,NULL),(10,3,4,'pending',NULL,NULL),(11,200,450,'pending',NULL,NULL),(12,600,300,'pending',2,NULL),(13,300,300,'confirmed',2,NULL),(14,280,200,'pending',2,NULL),(15,300,300,'pending',2,NULL),(16,300,300,'pending',2,NULL);
 /*!40000 ALTER TABLE `orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -65,15 +61,15 @@ CREATE TABLE `partlistitem` (
                                 `name` varchar(255) DEFAULT NULL,
                                 `quantity` int DEFAULT NULL,
                                 `length` double DEFAULT NULL,
-                                `unit` varchar(45) DEFAULT NULL,
+                                `unit` varchar(10) DEFAULT NULL,
                                 `description` varchar(255) DEFAULT NULL,
-                                `price` decimal(8,2) DEFAULT NULL,
+                                `price` int DEFAULT NULL,
                                 PRIMARY KEY (`id`),
                                 KEY `fk_partlists_parts1_idx` (`parts_id`),
                                 KEY `fk_order_id_idx` (`order_id`),
                                 CONSTRAINT `fk_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
                                 CONSTRAINT `fk_partlists_parts1` FOREIGN KEY (`parts_id`) REFERENCES `parts` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -82,6 +78,7 @@ CREATE TABLE `partlistitem` (
 
 LOCK TABLES `partlistitem` WRITE;
 /*!40000 ALTER TABLE `partlistitem` DISABLE KEYS */;
+INSERT INTO `partlistitem` VALUES (3,14,1,NULL,NULL,300,NULL,NULL,NULL),(4,14,3,NULL,NULL,280,NULL,NULL,NULL),(5,14,2,NULL,NULL,280,NULL,NULL,NULL),(6,15,1,NULL,NULL,300,NULL,NULL,NULL),(7,15,3,NULL,NULL,300,NULL,NULL,NULL),(8,15,2,NULL,NULL,300,NULL,NULL,NULL),(9,16,1,NULL,NULL,300,NULL,NULL,120),(10,16,3,NULL,NULL,300,NULL,NULL,151),(11,16,2,NULL,NULL,300,NULL,NULL,111);
 /*!40000 ALTER TABLE `partlistitem` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -176,4 +173,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-05-18 10:52:17
+-- Dump completed on 2021-05-19 13:56:24
