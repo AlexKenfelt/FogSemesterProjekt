5405720331b2caaec2f396995293e3cb756b917f
Julius <PsychedelicCoder@users.noreply.github.com>
2021-05-24 00:50:58 +0200

    BomFacade Cleanup

--- src/main/java/business/services/BomFacade.java
+++ src/main/java/business/services/BomFacade.java
@@ -1,32 +1,30 @@
 package business.services;
 
-
 import business.entities.CarportItems;
 import business.exceptions.UserException;
 import business.persistence.BomMapper;
 import business.persistence.Database;
-
-
 import java.util.List;
 
-public class BomFacade {
-
+public class BomFacade
+{
     BomMapper bomMapper;
 
-    public BomFacade(Database database) {
+    //The constructor.
+    public BomFacade(Database database)
+    {
         bomMapper = new BomMapper(database);
     }
 
-    public List<CarportItems> getBomByOrderId(Integer orderId) throws UserException {
+    //Get the Bill of materials on specific order id.
+    public List<CarportItems> getBomByOrderId(Integer orderId) throws UserException
+    {
         return bomMapper.getBomByOrderId(orderId);
     }
 
-
-
-
-    public double getSummedPrice(Integer orderId) throws UserException {
-
+    //Calculates the total price of a carport.
+    public double getSummedPrice(Integer orderId) throws UserException
+    {
         return bomMapper.summedPrice(orderId);
-
     }
 }
