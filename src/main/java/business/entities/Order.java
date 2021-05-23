c6ab4368903b42fe37db1cdac6333f5ce441ea52
Julius <PsychedelicCoder@users.noreply.github.com>
2021-05-23 19:44:58 +0200

    Order cleanup + Carportitems toString update

--- src/main/java/business/entities/Order.java
+++ src/main/java/business/entities/Order.java
@@ -2,15 +2,19 @@ package business.entities;
 
 import java.sql.Timestamp;
 
-public class Order {
+public class Order
+{
     private int id;
     private double width;
     private double length;
     private String status;
     private User user;
+
+    //TODO: Maybe not use this time format for our orders as it is giving problems.
     private Timestamp timestamp;
 
-    public Order(int id, double width, double length, String status, User user, Timestamp timestamp) {
+    public Order(int id, double width, double length, String status, User user, Timestamp timestamp)
+    {
         this.id = id;
         this.width = width;
         this.length = length;
@@ -19,13 +23,15 @@ public class Order {
         this.timestamp = timestamp;
     }
 
-    public Order(double width, double length) {
+    public Order(double width, double length)
+    {
         this.width = width;
         this.length = length;
         this.status = "pending";
     }
 
-    public Order(int id, double width, double length, String status, Timestamp timestamp) {
+    public Order(int id, double width, double length, String status, Timestamp timestamp)
+    {
         this.id = id;
         this.width = width;
         this.length = length;
@@ -34,64 +40,72 @@ public class Order {
         this.timestamp = timestamp;
     }
 
-
-    public int getId() {
+    public int getId()
+    {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(int id)
+    {
         this.id = id;
     }
 
-    public double getWidth() {
+    public double getWidth()
+    {
         return width;
     }
 
-    public void setWidth(double width) {
+    public void setWidth(double width)
+    {
         this.width = width;
     }
 
-    public double getLength() {
+    public double getLength()
+    {
         return length;
     }
 
-    public void setLength(double length) {
+    public void setLength(double length)
+    {
         this.length = length;
     }
 
-    public String getStatus() {
+    public String getStatus()
+    {
         return status;
     }
 
-    public void setStatus(String status) {
+    public void setStatus(String status)
+    {
         this.status = status;
     }
 
-    public User getUser() {
+    public User getUser()
+    {
         return user;
     }
 
-    public void setUser(User user) {
+    public void setUser(User user)
+    {
         this.user = user;
     }
 
-    public Timestamp getTimestamp() {
+    public Timestamp getTimestamp()
+    {
         return timestamp;
     }
 
-    public void setTimestamp(Timestamp timestamp) {
-        this.timestamp = timestamp;
-    }
-
     @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", width=" + width +
-                ", length=" + length +
-                ", status='" + status + '\'' +
-                ", user=" + user +
-                ", timestamp=" + timestamp +
-                '}';
+    public String toString()
+    {
+        return 
+        (
+                "id = " + id +
+                ", width = " + width +
+                ", length = " + length +
+                ", status = " + status +
+                ", user = " + user +
+                ", timestamp = " + timestamp
+        );
     }
 }
