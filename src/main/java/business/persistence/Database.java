043c567ff10b09150d1695982180b0fc1d10d4ef
Julius <PsychedelicCoder@users.noreply.github.com>
2021-05-23 20:22:38 +0200

    Database class cleanup

--- src/main/java/business/persistence/Database.java
+++ src/main/java/business/persistence/Database.java
@@ -4,20 +4,24 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Database {
-    private Connection connection;
+public class Database
+{
     private final String USER;
     private final String PASSWORD;
     private final String URL;
 
-    public Database(String user, String password, String url) throws ClassNotFoundException {
+    public Database(String user, String password, String url) throws ClassNotFoundException
+    {
         String deployed = System.getenv("DEPLOYED");
-        if (deployed != null) {
+        if (deployed != null)
+        {
             // Prod: hent variabler fra setenv.sh i Tomcats bin folder
             USER = System.getenv("JDBC_USER");
             PASSWORD = System.getenv("JDBC_PASSWORD");
             URL = System.getenv("JDBC_CONNECTION_STRING");
-        } else {
+        }
+        else
+        {
             USER = user;
             PASSWORD = password;
             URL = url;
@@ -25,9 +29,10 @@ public class Database {
         Class.forName("com.mysql.cj.jdbc.Driver");
     }
 
-    public Connection connect() throws SQLException {
-        Connection connection = null;
-        connection = DriverManager.getConnection(URL, USER, PASSWORD);
-        return connection;
+    public Connection connect() throws SQLException
+    {
+        Connection con = null;
+        con = DriverManager.getConnection(URL, USER, PASSWORD);
+        return con;
     }
 }
