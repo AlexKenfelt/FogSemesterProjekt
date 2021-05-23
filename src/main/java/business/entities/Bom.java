88d093ee2bbbd7d38b15ccf55df12fe784a8a6f4
Julius <PsychedelicCoder@users.noreply.github.com>
2021-05-23 18:34:15 +0200

    Bom cleanup

--- src/main/java/business/entities/Bom.java
+++ src/main/java/business/entities/Bom.java
@@ -3,11 +3,9 @@ package business.entities;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Bom {
-
-    //CarportItems carportItems;
-
-
+public class Bom
+{
+    //This is our list that contains the items for our Bill of materials.
     private List<CarportItems> bomLines;
 
     public Bom()
@@ -15,13 +13,13 @@ public class Bom {
         this.bomLines = new ArrayList<>();
     }
 
-    public List<CarportItems> getCarportItems() {
-
+    public List<CarportItems> getCarportItems()
+    {
         return bomLines;
     }
 
-    public void addToBill (CarportItems carportItems){
+    public void addToBill(CarportItems carportItems)
+    {
         bomLines.add(carportItems);
     }
-
 }
