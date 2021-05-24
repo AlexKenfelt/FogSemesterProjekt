93d81f772c2303d0b6993b9bfd67a26b7cb87132
Julius <PsychedelicCoder@users.noreply.github.com>
2021-05-25 01:35:29 +0200

    CreateOrderCommand cleanup

--- src/main/java/web/commands/CreateOrderCommand.java
+++ src/main/java/web/commands/CreateOrderCommand.java
@@ -1,28 +1,20 @@
 package web.commands;
 
-import business.entities.Bom;
-import business.entities.Order;
-import business.entities.User;
 import business.exceptions.UserException;
-import business.services.BomService;
-import business.services.OrderFacade;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-public class CreateOrderCommand extends CommandProtectedPage {
+public class CreateOrderCommand extends CommandProtectedPage
+{
 
-    OrderFacade orderFacade;
-
-
-    public CreateOrderCommand(String pageToShow, String role) {
+    public CreateOrderCommand(String pageToShow, String role)
+    {
         super(pageToShow, role);
     }
 
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException {
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException
+    {
         return pageToShow;
     }
-
 }
