08ab14df0d3d80d0537965e0a85ed6f7814a34a4
Julius <PsychedelicCoder@users.noreply.github.com>
2021-05-25 01:44:47 +0200

    CustommerOrderCommand cleanup

--- src/main/java/web/commands/CustomerOrderCommand.java
+++ src/main/java/web/commands/CustomerOrderCommand.java
@@ -4,39 +4,35 @@ import business.entities.Order;
 import business.entities.User;
 import business.exceptions.UserException;
 import business.services.OrderFacade;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
-public class CustomerOrderCommand extends CommandProtectedPage {
-
+public class CustomerOrderCommand extends CommandProtectedPage
+{
     OrderFacade orderFacade;
 
-    public CustomerOrderCommand (String pageToShow, String status) {
+    public CustomerOrderCommand (String pageToShow, String status)
+    {
         super(pageToShow, status);
         this.orderFacade = new OrderFacade(database);
     }
 
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException {
-
-        //Her henter vi User_ID fra vores session scope så det passer på den user der er logget ind.
-
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException
+    {
         HttpSession session = request.getSession();
 
+        //Get UserID from session.
         User user = (User) session.getAttribute("user");
 
+        //Getting customers order based on session customerID
         List<Order> orders = orderFacade.getOrderByCustomerId(user.getId());
-        //List<Order> status = orderFacade.getOrderStatus(user.getId());
-        
+
+        //Setting that list of users order to the sessionScope.
         session.setAttribute("orders", orders);
-        //session.setAttribute("status",status);
 
         return pageToShow;
     }
-
 }
