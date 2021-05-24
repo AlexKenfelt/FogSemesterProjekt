5030fa632635383d100120739f9a009ebfefe0dd
Julius <PsychedelicCoder@users.noreply.github.com>
2021-05-24 02:06:55 +0200

    BomService Cleanup

--- src/main/java/business/services/BomService.java
+++ src/main/java/business/services/BomService.java
@@ -5,98 +5,100 @@ import business.entities.Parts;
 import business.exceptions.UserException;
 import business.persistence.Database;
 
-public class BomService {
-
+public class BomService
+{
     PartsFacade partsFacade;
 
-    public BomService(Database database) {
+    public BomService(Database database)
+    {
         this.partsFacade = new PartsFacade(database);
     }
 
-    public CarportItems calculatePosts ( double length) throws UserException {
-
+    //Here we calculate the number of posts needed based on length of the carport.
+    public CarportItems calculatePosts ( double length) throws UserException
+    {
+        //Pulling the parts id from the database.
         Parts parts = partsFacade.getPartsById(1);
         int parts_id = parts.getId();
 
+        //Hardcoded parts info.
         String name = "25x200mm. trykimp. Brædt";
         String unit = "stk.";
         String desc = "This is a description of how to use this item.";
+
+        //The variables.
         int price;
         int quantity = 4;
-
-        //Den foreste stolpe skal sidde en meter inde.
         int frontPost = 100;
-        //Den bagerste stolpe skal sidde 20 cm meter inde.
         int backPost = 20;
-        //dette er den faktiske længde efter vi har minusset med 'frontpost' og 'backpost'.
         int actualLength = 0;
-        //Dette er minimums kravet for hvor meget plads der skal være mellem hver stolpe.
         int minimumPostSpacing = 310;
-        //Dette er bare en konstant.
         int multiplier = 2;
-        //dette er vores float som kun bliver brugt i mellem regningerne, denne bliver typecasted til int result længere nede.
         double result = 0;
-        //Her vælger vi og tage carportens indtastede længde og minuser det med, det stykke som den foreste og bagerste stolpe skal sidde inde.
-        actualLength = ((int)length - frontPost - backPost);
-        //Her tager vi minimums kravet til spacing mellem hver stolpe og dividerer det med den faktiske længde på vore carport
-        result = ( actualLength / minimumPostSpacing);
-        //her tager vi og runder result op til det tætteste hel tal.
-        result = Math.round(result);
-        //Her ganger vi med 2 så vi får begge sider med af carporten.
-        quantity = quantity + ((int) result * multiplier);
-        //Nu typecaster vi vores result som er en float om til en int.
-        int numberOfPosts = (int)result ;
-        //Dett her er det endelige resultat.
 
-        //Prisen er ligenu bare hardcodet. kunne være sjovt og lave en metode der henter prisen ud fra parts id.
+        //The calculations.
+        actualLength = ((int)length - frontPost - backPost);
+        result = ( actualLength / minimumPostSpacing);
+        result = Math.round(result);
+        quantity = quantity + ((int) result * multiplier);
         price = (quantity * 100);
 
+        //Creating an object that contains the data.
         CarportItems tmpCarportItems = new CarportItems(parts_id, name, quantity, length, unit, desc, price);
-
         return tmpCarportItems;
     }
 
-    public CarportItems calculateBeams (double length) throws UserException {
-
+    //Here we calculate the number of Beams needed based on length of the carport.
+    public CarportItems calculateBeams (double length) throws UserException
+    {
+        //Pulling the parts id from the database.
         Parts parts = partsFacade.getPartsById(2);
         int parts_id = parts.getId();
 
+        //Hardcoded parts info.
         String name = "25x200mm. trykimp. Brædt";
         String unit = "stk.";
         String desc = "This is a description of how to use this item.";
+
+        //The variables.
         int price;
         int quantity = 2;
+        int lengthOfBeams;
 
-        int lengthOfBeams = (int) length;
-
+        //The calculations.
+        lengthOfBeams = (int) length;
         price = (quantity * 75);
 
+        //Creating an object that contains the data.
         CarportItems tmpCarportItems = new CarportItems(parts_id, name, quantity, lengthOfBeams, unit, desc, price);
-
         return tmpCarportItems;
     }
 
-    public CarportItems calculateRafters (double width, double length) throws UserException {
-
+    //Here we calculate the number of rafters needed based on length and width of the carport.
+    public CarportItems calculateRafters (double width, double length) throws UserException
+    {
+        //Pulling the parts id from the database.
         Parts parts = partsFacade.getPartsById(3);
         int parts_id = parts.getId();
+
+        //Hardcoded parts info.
         String name = "25x200mm. trykimp. Brædt";
         String unit = "stk.";
         String desc = "This is a description of how to use this item.";
+
+        //The variables.
         int price;
         int quantity;
-
-        //Dette er afstanden der skal være imellem hver spær.
         double rafterSpacing = 55;
-        //Dette er længden af vores spær. Dette skal helst være lig med den længde der bliver givet til carporten.
-        int lengthOfRafters = (int) width;
-        //Vi tager bare og dividerer længden med, den bestemte afstand der skal være, for at få antal spær.
-        quantity = (int) (length / rafterSpacing);
+        int lengthOfRafters;
 
+        //The calculations.
+        lengthOfRafters = (int) width;
+        quantity = (int) (length / rafterSpacing);
         price = (quantity * 115);
 
+        //Creating an object that contains the data.
         CarportItems tmpCarportItems = new CarportItems(parts_id, name, quantity, lengthOfRafters, unit, desc, price);
-
         return tmpCarportItems;
     }
 }
