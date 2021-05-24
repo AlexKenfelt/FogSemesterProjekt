17f225dbb34df47a9b878ebc35d026382830f046
Julius <PsychedelicCoder@users.noreply.github.com>
2021-05-25 01:51:15 +0200

    RecieptPage cleanup

--- src/main/java/web/commands/ReceiptPage.java
+++ src/main/java/web/commands/ReceiptPage.java
@@ -1,26 +1,28 @@
 package web.commands;
 
 import business.exceptions.UserException;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ReceiptPage extends Command {
+public class ReceiptPage extends Command
+{
     public String role;
     public String pageToShow;
 
-    public ReceiptPage(String pageToShow, String role) {
+    public ReceiptPage(String pageToShow, String role)
+    {
         this.pageToShow = pageToShow;
         this.role = role;
-
     }
 
     @Override
-    public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException {
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException
+    {
         return pageToShow;
     }
 
-    public String getRole() {
+    public String getRole()
+    {
         return role;
     }
 }
